202b3d27bdff56191f8fff7abd2ba658d65d5326
Jardel de Castro Queiroz <jardeljcadm@gmail.com>
2023-07-19 15:27:22 -0300

    Update Fatorial.cpp

--- Projeto em c++/Fatorial.cpp
+++ Projeto em c++/Fatorial.cpp
@@ -4,7 +4,7 @@ int main (void)
 {
 	int fatorial = 1;
 	int numero;
-	printf("Digite o número para fazer seu fatorial\n");
+	printf("Digite o nÃºmero para fazer seu fatorial\n");
 	scanf("%i", &numero);
 	 while (numero > 0)
 	 	{
@@ -23,7 +23,7 @@ int main (void)
 {1
 	int fatorial = 1;
 	int numero;
-	printf("Digite o número para fazer seu fatorial\n");
+	printf("Digite o nï¿½mero para fazer seu fatorial\n");
 	scanf("%i", &numero);
 	for (;numero > 0; numero--)
 	 	{
